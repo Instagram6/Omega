b7cde9df7d2d7c95ae76948bead0c84028bc8069
Instagram6 <69061245+Instagram6@users.noreply.github.com>
2020-08-01 08:53:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 
 ///Edit Under This
 const firebaseConfig = {
-  apiKey: "AIzaSyAbEkpl7DxWXTliwyK9VhFuC2U0ExZNsLk",
-  authDomain: "instagram-afbe6.firebaseapp.com",
-  databaseURL: "https://instagram-afbe6.firebaseio.com",
-  projectId: "instagram-afbe6",
-  storageBucket: "instagram-afbe6.appspot.com",
-  messagingSenderId: "925316947305",
-  appId: "1:925316947305:web:64366356bb0a3656a1f1f8",
-  measurementId: "G-H1S0X5KJ7D"
+  apiKey: "AIzaSyCxrOPJxIbWGaaSkam21pF1x3X6RZcq4jY",
+  authDomain: "instagram-682be.firebaseapp.com",
+  databaseURL: "https://instagram-682be.firebaseio.com",
+  projectId: "instagram-682be",
+  storageBucket: "instagram-682be.appspot.com",
+  messagingSenderId: "53035667476",
+  appId: "1:53035667476:web:c3d65a2302b5a206f61701",
+  measurementId: "G-BP7KCSKEF5"
 };
 
      
